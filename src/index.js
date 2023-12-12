bf9a0e61fe8ce2b60025b3cfbf38ae2cad611bd7
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2023-12-12 13:03:53 -0700

    push blog project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Main from './pages/Main'
+import Blog from './pages/Blog'
+import Edit from './pages/Edit'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+  },
+  {
+    path: "/edit",
+    element: <Edit/>,
+  },
+  {
+    path: "/main",
+    element: <Main/>,
+  },
+  {
+    path: "/blog",
+    element: <Blog/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
